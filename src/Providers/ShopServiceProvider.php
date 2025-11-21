9d6b18ada6a93bd2d5060bc747f1bbfa3dbc4e73
Molitor István <istvan.molitor@centralmediacsoport.hu>
2025-11-22 00:43:06 +0100

    init

---
+++ src/Providers/ShopServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Molitor\Shop\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ShopServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'shop');
+    }
+
+    public function register()
+    {
+
+    }
+}
