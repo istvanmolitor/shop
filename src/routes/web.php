64f3bae40858115a42ac4a8866ea1e8009d726cc
Molitor István <istvan.molitor@centralmediacsoport.hu>
2025-11-22 01:29:42 +0100

    show product

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Molitor\Shop\Http\Controllers\ShopProductController;
 
-Route::get('/shop/products', [ShopProductController::class, 'index'])->name('shop.products.index');
-Route::get('/shop/products/{product:slug}', [ShopProductController::class, 'show'])->name('shop.products.show');
+Route::middleware('web')->group(function () {
+    Route::get('/shop/products', [ShopProductController::class, 'index'])->name('shop.products.index');
+    Route::get('/shop/products/{product:slug}', [ShopProductController::class, 'show'])->name('shop.products.show');
+});
